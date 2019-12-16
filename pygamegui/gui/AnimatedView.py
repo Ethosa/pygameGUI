7417a3b2547562235ed900c662daf9b96fc04716
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-12-16 22:49:27 +0700

    added new docstrings

--- pygamegui/gui/AnimatedView.py
+++ pygamegui/gui/AnimatedView.py
@@ -18,6 +18,8 @@ class AnimatedView(View):
             width {number} -- view width (default: {100})
             height {number} -- view height (default: {100})
             background_color {tuple} -- backgrond color (default: {(255, 255, 255, 255)})
+            frames {list} -- frames for animation (default: {[]})
+            animation_speed {number} -- frame rate
         """
         View.__init__(self, parent, width, height, background_color)
         self.frames = frames
@@ -25,6 +27,11 @@ class AnimatedView(View):
         self.state = 0.0
 
     def add_frame(self, frame):
+        """add new frame in animation frames
+
+        Arguments:
+            frame {tuple or pygame.Surface} -- color or surface
+        """
         if isinstance(frame, tuple):
             color = frame[:]
             frame = Surface((self.width, self.height)).convert_alpha()
@@ -32,6 +39,8 @@ class AnimatedView(View):
         self.frames.append(frame)
 
     def animate(self):
+        """change frame
+        """
         if len(self.frames) > 1:
             if self.state < len(self.frames)-self.animation_speed:
                 self.state += self.animation_speed
